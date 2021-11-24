6e63ff9b0d70dad635e9018503f800f652cd5ccf
dallanj <dallanjones@pm.me>
2021-11-24 12:22:15 -0500

    updating production branch

--- src/App (3rd copy).js
+++ src/App (3rd copy).js
@@ -1,17 +1,10 @@
 import './App.css';
 import { useEffect, useImperativeHandle, useState } from 'react';
-import firebase from './firebase';
-import raw from './list.txt';
+import firebase from './firebase.js';
 import { createPortal } from 'react-dom';
 
 
 function App() {
-
-  // welcome wizard
-  const [wizard, setWizard] = useState('');
-  const [user, setUser] = useState('');
-  const [importUid, setImportUid] = useState('');
-
   // income stuff
   const [incomes, setIncome] = useState([]);
   const [userInputIncome, setUserInputIncome] = useState({
@@ -37,7 +30,7 @@ function App() {
 
   // function to create a unique uid
   const uid = function(){
-    return 'budgeter-' + Date.now().toString(36) + Math.random().toString(36).substr(2);
+    return Date.now().toString(36) + Math.random().toString(36).substr(2);
   }
 
   /*
@@ -56,8 +49,6 @@ function App() {
 
     // using localstorage will set a unique userId to be used for a unique user experience
     localStorage.setItem("userId", userId);
-
-    console.log(userId.length)
   }
   // store userId in a variable for later use
   const userId = localStorage.getItem("userId");
@@ -75,7 +66,7 @@ function App() {
   if(!localStorage.getItem("budgeterUser")) {
 
     // using localstorage will set a unique userId to be used for a unique user experience
-    localStorage.setItem("budgeterUser", 'Stranger');
+    localStorage.setItem("budgeterUser", 'User');
   }
   // store userId in a variable for later use
   const username = localStorage.getItem("budgeterUser");
@@ -85,13 +76,13 @@ function App() {
   
 
   useEffect(() => {
-
     // make ref to database
     const dbRef = firebase.database().ref();
 
     dbRef.on('value', (response) => {
+      // console.log(response.val());
 
-      // variables to store new state
+      // var to store new state
       const newIncome = [];
       const arrayOfIncomes = [];
       let sumIncome = 0;
@@ -104,7 +95,13 @@ function App() {
       if(data == null) {
         data = [];
       }
+      // console.log(data[userId])
 
+      // console.log('hasd')
+      // calculate sum total income
+
+      // console.log(data[userId]['-Mop_uub7svdqp5KjZx1'].amount)
+      // if(typeof data[userId] !== null) {
       // iterate through database data where index = userId
         for(let property in data[userId]) {
 
@@ -117,28 +114,41 @@ function App() {
             arrayOfExpenses.push(parseInt(data[userId][property].amount));
           }
 
-          // push to newIncome array if income
-          if(data[userId][property].type === 'income') {
-            newIncome.push({
-              id: property,
-              type: data[userId][property].type,
-              date: data[userId][property].date,
-              description: data[userId][property].description,
-              amount: data[userId][property].amount
-          })
+              // push to newIncome array if income
+            if(data[userId][property].type === 'income') {
+              newIncome.push({
+                id: property,
+                type: data[userId][property].type,
+                date: data[userId][property].date,
+                description: data[userId][property].description,
+                amount: data[userId][property].amount
+              })
+              // const amountInt = parseInt(data[userId][property].amount)
+              // // console.log('is it num or string')
+              // // console.log(typeof poop)
+              
+              // setTotalIncome(totalIncome + amountInt)
+            // push to newExpense array if expense
+            } else {
+              newExpense.push({
+                id: property,
+                type: data[userId][property].type,
+                date: data[userId][property].date,
+                description: data[userId][property].description,
+                amount: data[userId][property].amount
+              })
+            }
+        
+            
 
-          // push to newExpense array if expense
-          } else {
-            newExpense.push({
-              id: property,
-              type: data[userId][property].type,
-              date: data[userId][property].date,
-              description: data[userId][property].description,
-              amount: data[userId][property].amount
-            })
-          }      
+        
+              // in order to control for the async nature of the state updater function, you can pass in a callback function
+                  //use the MOST recent value of state FIRST before moving on to a subsequent value
+              
+        
+          
+        
         }
-
         // for array of incomes
         for (let i = 0; i < arrayOfIncomes.length; i++) {
           sumIncome += arrayOfIncomes[i]
@@ -156,7 +166,10 @@ function App() {
         // total sums of income and expenses
         setTotalSavings(sumIncome - sumExpense);
 
-    }) 
+    })
+
+
+    
 
   }, []);
 
@@ -204,6 +217,7 @@ function App() {
     const cDay = currentDate.getDate();
     const cMonth = currentDate.getMonth() + 1;
     const cYear = currentDate.getFullYear();
+    
 
     // push expense to db only if properties are not empty
     if(userInputExpense.type === 'expense') {
@@ -271,141 +285,76 @@ function App() {
     })
   }
 
-  // handle submit for adding income or expense
   const handleSubmit = (event) => {
     event.preventDefault();
     
+    // console.log('below is target')
+    // console.log(event.target[1].value);
+    // console.log(event.target[4].value)
     // make db connection
     const dbRef = firebase.database().ref(`/${userId}`);
 
     if(event.target[1].value == 'income') {
       dbRef.child(event.target[0].value).push(userInputIncome);
       console.log('its an income')
+
+      // setTotalIncome(totalIncome+parseInt(event.target[4].value))
+      
+      // setTotalIncome(totalIncome+)
     }
     else if(event.target[1].value == 'expense')
     {
       dbRef.child(event.target[0].value).push(userInputExpense);
       console.log('its an expense')
     }
-  }
 
-  // update the username field in the welcome wizard
-  const handleUserChange = (event) => {
-    const target = event.target;
-    const value = target.value;
-    const name = target.name;
+    // old shit that works DONT FUCK IT UP
+    // if(event.target[1].value == 'income') {
+    //   dbRef.child(event.target[0].value).set(userInputIncome);
+    //   console.log('its an income')
 
-    // set the data to user
-    setUser(value);
-  }
-
-  // handle submit for adding a username
-  const handleUserSubmit = (event) => {
-    event.preventDefault();
-
-    // check for errors within the wizard
-    let errorCheck = 'false';
-
-    // initiate the items array to avoid errors
-    // let items = [];
-
-    // fetch the imported profanity list >:)
-    fetch(raw)
-    .then(r => r.text())
-    .then(text => {
+    //   setTotalIncome(totalIncome+parseInt(event.target[4].value))
       
-      // create an items array which adds each word by new line
-      const items = text.split("\n")
+    //   // setTotalIncome(totalIncome+)
+    // }
+    // else if(event.target[1].value == 'expense')
+    // {
+    //   dbRef.child(event.target[0].value).set(userInputExpense);
+    //   console.log('its an expense')
+    // }
 
-      // check if username contains a bad word
-      if(items.indexOf(user) >= 0 || user.trim() == '') {
-        // create error message and exit since we found a bad word
-        errorCheck = 'true';
-        setError('name', 'usernameLabel', 'username', 'No naughty words pal, nice try')
-      }
-      // else  // set the username to localstorage if there is no profanity
-      // {
-      //   errorCheck = 'false';
-      // }
+    // console.log('below is contents of userInputIncome')
+    // console.log(userInputIncome);
 
-      // if there are no errors (username doesn't contain profanity)
-      if(errorCheck === 'false') {
-        localStorage.setItem("budgeterUser", user);
-
-        // set the importUid if not an empty string
-        if(importUid.trim() != '' && importUid.length === 28) {
-          localStorage.setItem("userId", importUid);
-        }
-
-        // set the data to user
-        setWizard('off');
-
-        // set the welcome wizard to 'off'
-        if(errorCheck === 'false') {
-          localStorage.setItem("welcomeWizard", 'off');
-
-          // set the data to user
-          window.location.reload(true)
-        }
-      }
-
-    });
     
+
+    // store data passed from add button
+    // const type = event.target.value;
+    // const key = uid();
+
+    // // create a timestamp
+    // const currentDate = new Date();
+    // const cDay = currentDate.getDate();
+    // const cMonth = currentDate.getMonth() + 1;
+    // const cYear = currentDate.getFullYear();
+
+    // if(type === 'income') {
+      
+    // } 
+    // else {
+    //   dbRef.update({
+    //     // id: `expense${key}`,
+    //     type: type,
+    //     date: `${cDay}/${cMonth}/${cYear}`,
+    //     description: "description",
+    //     amount: "0"
+    //    });
+    // }
+    
+
   }
 
-    // import an existing uid field in the welcome wizard
-    const handleImportUid = (event) => {
-      const target = event.target;
-      const value = target.value;
-      const name = target.name;
-  
-      // set the data to user
-      setImportUid(value);
-    }
-
-    function setError(firstId, secondId, thirdId, message) {
-      // remove existing error
-      const div = document.getElementById(firstId);
-      secondId = document.getElementById(secondId);
-      thirdId = document.getElementById(thirdId);
-
-      /* 
-      to avoid any duplicates im checking if an error is present 
-      on the page by checking the document if the class of errorMsg exists
-      */
-      if(!document.getElementsByClassName('errorMsg').length) {
-        thirdId.style.background = '#fbe7f0'
-
-        // since im using 2 grid columns, I need an empty div below the label
-        const emptyNode = document.createElement("div");
-  
-        // create div which will contain the error
-        const node = document.createElement("div");
-        node.classList.add('errorMsg');
-  
-        node.innerText = message;
-  
-        // append message to new node and append node to parent
-        document.getElementById(firstId).appendChild(emptyNode); 
-        document.getElementById(firstId).appendChild(node);
-      } 
-  }
-
-  function unsetError(firstId, secondId) {
-    const div = document.getElementById(firstId);
-
-    /*
-        if div has more than 1 child
-        child[0] is the input
-        anything greater needs to be removed
-        and background of the input needs to changed back to non error state
-
-    */
-    if(div.children.length > 1) {
-        div.removeChild(div.lastChild);
-    }
-    secondId.style.background = '#f6f6f6';
-}
+  // console.log(incomes);
 
   return (
     <div className="App">
@@ -420,52 +369,11 @@ function App() {
           
           {/* Welcome wizard menu */}
           <div className="wizardMenu">
-            <h2 className="wizard">Welcome to Budgeter!</h2>
-
-            <p className="wizardopac">
-              Greetings, <span className="italic bold">stranger</span>!
-            </p>
-
-            <p className="wizardopac">
-              Within the toolbox menu you have the option to either add an income or expense to your budget sheet by simply filling out one of the forms with a description and an amount followed by clicking <span className="italic bold">"Add income"</span> or <span className="italic bold">"Add expense"</span>
-            </p>
-
-            <p className="wizardopac">
-              The summary menu will summarize and calculate your <span className="italic bold">total savings</span> by taking your total income and subtracting your expenses to give you the final balance.
-            </p>
-
-            <p className="wizardopac">
-              Pretty straight forward, right? Before we begin tracking our finances, let us know what your prefered name is!
-            </p>
-
-            <form onSubmit={handleUserSubmit}>
-
-              <div className="grid opac">
-                <div className="italic">Items with (*) are required</div>
-                <div></div>
-              </div>
-
-              <div id="name" className="grid twohalf wizardopac">
-                <label id="usernameLabel" for="username"><span className="bold">Prefered name (*)</span></label>
-                <input className="wizardInput" id="username" name="username" type="text" value={user} onChange={handleUserChange} placeholder="Username"></input>
-              </div>
-
-              <div className="grid twohalf wizardopac">
-                <label for="importUid"><span className="bold">Import existing Budgeter ID</span></label>
-                <input className="wizardInput" id="importUid" name="importUid" type="text" value={importUid} onChange={handleImportUid} placeholder="budgeter-kwcot6uzyz41njxvwkn"></input>
-              </div>
-
-              <div className="grid three">
-                <div></div>
-                <div></div>
-                <button className="wizardButton bold">Finish</button>
-              </div>
-
-            </form>
+            <h1 className="wizard">Welcome to Budgeter!</h1>
           </div>
           </>
         ) : (
-          <div></div>
+          <div>s</div>
         )
       }
 
@@ -473,14 +381,7 @@ function App() {
       
       
       <h2 className="greeting">Hi {username} 👋</h2>
-
-      {
-        welcomeWizard === 'on' ? (
-          <h1 className="greeting">Welcome to Budgeter!</h1>
-        ) : (
-          <h1 className="greeting">Welcome back to Budgeter!</h1>
-        )
-      }
+      <h1 className="greeting">Welcome back to Budgeter!</h1>
 
       <main className="row">
         <div className="table settings">
@@ -516,9 +417,28 @@ function App() {
               </form>
             </div>
 
-          </div>
-      
+            {/* row 3 */}
+            {/* <div>
+              <form onSubmit={addRecord}>
+                <button className="actionButton" value='income' onClick={addRecord} id="newIncome">Add income</button>
+              </form>
+            </div>
+            <div>
+              <form onSubmit={addRecord}>
+                <button className="actionButton" value='expense' onClick={addRecord} id="newExpense">Add expense</button>
+              </form>
+            </div>
+            <div></div> */}
 
+          </div>
+
+          
+        {/* <label htmlFor="newBook">Add a new book to the shelf</label> */}
+        {/* <input id="value="income" type="text"/> */}
+        
+        
+        {/* <button value='expense' onClick={addRecord} id="newExpense">Add expense</button> */}
+      
         </div>
 
         <div className="table settings">
@@ -609,6 +529,9 @@ function App() {
             </div>
           
             {
+              
+             
+
               expenses.map((expense) => {
                 return (
                   <form onSubmit={handleSubmit}>
@@ -624,6 +547,33 @@ function App() {
             }
           </fieldset>
         </div>
+
+        {/* <div> */}
+
+          {/* display expense data */}
+          {/* <h2 className="bottom">Expense</h2>
+            {
+              
+              expenses.map((expense) => {
+                return (
+                  <form onSubmit={handleSubmit}>
+                    <div className="flex">
+                        <input hidden id="type" type="text" name="id" value={userInputExpense.id = expense.id} onChange={handleExpenseChange} />
+                        <input hidden id="type" type="text" name="type" value={userInputExpense.type = expense.type} onChange={handleExpenseChange} />
+                        <div><input disabled id="date" type="text" name="date" value={userInputExpense.date = expense.date} onChange={handleExpenseChange} /></div>
+                        <div><input id="description" type="text" name="description" value={userInputExpense.description} onChange={handleExpenseChange} /></div>
+                        newIncome works but i need to name it amount
+                        <div><input id="amount" type="text" name="amount" value={userInputExpense.amount} onChange={handleExpenseChange} /></div>
+                        <div><button className="changeButton"><i className="fas fa-check"></i></button></div>
+                        <div><button className="changeButton" onClick={ () => removeRow(expense.id) }><i className="fas fa-times"></i></button></div> 
+                      <button onClick={ () => removeBook(Expense.incomeId) }>remove book</button>
+                    </div>
+                  </form>
+                )
+              })
+            }
+          </div> */
+          }
 
       </section>
 
